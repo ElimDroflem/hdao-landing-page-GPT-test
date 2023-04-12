b80b170a77d46397b68726db39cf39ddbd17185a
ElimDroflem <105154979+ElimDroflem@users.noreply.github.com>
2023-04-12 18:39:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("bookCallBtn").addEventListener("click", bookCall);
+document.getElementById("bookCallLink").addEventListener("click", bookCall);
+
+function bookCall() {
+  alert(
+    "Thank you for your interest. Our team will reach out to you shortly to schedule a call."
+  );
+}
